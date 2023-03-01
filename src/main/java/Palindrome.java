786338223fcd9c1a8276789601a3ff4a0d1627e1
Rey V <radiativedreams@gmail.com>
2023-03-01 02:43:59 +0000

    update

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -12,6 +12,7 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
+        // need to go over why stringbuilder did not work
         for(int i = 0, j = str.length()-1; i < str.length()/2; i++, j--)
         {
             if(str.charAt(i) != str.charAt(j)) return false;
